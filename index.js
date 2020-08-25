8b369d6e179cb63157fe61de3e5796477458fdc9
Phu Nguyen <phu.nguyen@aller.com>
2020-08-25 16:59:30 +0300

    feat: added codepush to app

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { registerRootComponent } from 'expo';
+import codePush from "react-native-code-push";
 
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(codePush(App));
